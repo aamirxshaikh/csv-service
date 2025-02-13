7c52a932035d4a99d303e21c5074bd7352ac7e24
aamirxshaikh <aamirshaikh3232@gmail.com>
2025-02-13 23:07:08 +0530

    Add validation to uploadMultipartFile method and improve exception handling

--- src/main/java/com/example/demo/controller/CsvController.java
+++ src/main/java/com/example/demo/controller/CsvController.java
@@ -1,6 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.exception.CsvProcessingException;
+import com.example.demo.exception.FileProcessingException;
 import com.example.demo.service.CsvService;
+import com.opencsv.exceptions.CsvException;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -11,28 +16,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+
 @RestController
 @RequestMapping("/api/csv")
 public class CsvController {
-    private static final Logger log = LoggerFactory.getLogger(CsvController.class);
-    private final CsvService csvService;
+  private static final Logger log = LoggerFactory.getLogger(CsvController.class);
+  private final CsvService csvService;
 
-    public CsvController(CsvService csvService) {
-        this.csvService = csvService;
+  public CsvController(CsvService csvService) {
+    this.csvService = csvService;
+  }
+
+  @PostMapping("/upload")
+  public ResponseEntity<String> uploadMultipartFile(@RequestParam("file") @Valid @NotEmpty MultipartFile file) {
+    log.info("Received file upload request.");
+
+    try {
+      csvService.uploadMultipartFile(file);
+      log.info("CSV file processed successfully and data persisted.");
+      return new ResponseEntity<>("CSV file uploaded and data persisted successfully!", HttpStatus.OK);
+    } catch (IOException e) {
+      throw new FileProcessingException("Error processing file " + e.getMessage());
+    } catch (CsvException e) {
+      throw new CsvProcessingException("Error processing CSV file: " + e.getMessage());
     }
-
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadMultipartFile(@RequestParam("file") MultipartFile file) {
-        log.info("Received file upload request.");
-
-        try {
-            log.info("Starting to process the CSV file...");
-            csvService.uploadMultipartFile(file);
-            log.info("CSV file processed successfully and data persisted.");
-            return new ResponseEntity<>("CSV file uploaded and data persisted successfully!", HttpStatus.OK);
-        } catch (Exception e) {
-            log.error("Error while uploading or processing CSV file: {}", e.getMessage(), e);
-            return new ResponseEntity<>("Error while uploading CSV file: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
+  }
 }
